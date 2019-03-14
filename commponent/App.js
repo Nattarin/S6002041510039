52fb394279395b0ba4c706c5b1d4917a9ff9cacc
ณัฐรินทร์ อินมณี <s6002041510039@Macmini-213-24.local>
2019-03-14 17:33:03 +0700

    create App component

---
+++ commponent/App.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import { View, Text } from 'react-native';
+
+class App extends React.Component {
+    render() {
+        return (
+                <View>
+                    <Text>Nattarin Inmanee</Text>
+                </View>
+        ); 
+    }
+}
+
+export default App;
